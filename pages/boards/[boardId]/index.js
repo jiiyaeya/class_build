745c525c480ffbbd2d9682f0ae20dc77f9e19fc4
jiiyaeya <jiiyaeya@naver.com>
2023-05-04 11:04:07 +0900

    빌드아이디 통일

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -9,3 +9,9 @@ export default function BoardPage() {
     </>
   );
 }
+// //서버사이드렌더링
+// export const getServerSideProps = () => {
+//   // 만약 서버사이드 렌더링 하는 페이지라면?
+//   // => out폴더로 생성 불가
+//   // => 이런경우, next.config.json에서 exportPathMap을 사용해서 현페이지 제외시키기
+// };
